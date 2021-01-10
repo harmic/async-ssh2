33e12ab4f6614f5859a84fd40fefa059a0a78320
eharmic <michael.harris@ericsson.com>
2021-01-11 10:41:30 +1100

    Re-write AsyncRead/AsyncWrite functions so they are written using Async<T>::poll_read and poll_write.
    Rewrite run_ssh2_fn so it uses
    session.block_directions to determine which
    direction will be waited on.
    
    Use of block_directions also means we need
    a reference to the session in channel, agent,
    listener and sftp objects.

--- src/listener.rs
+++ src/listener.rs
@@ -1,24 +1,26 @@
-use crate::{channel::Channel, util::run_ssh2_fn, Error};
+use crate::{channel::Channel, Error};
 use async_io::{Async, Timer};
 use ssh2::{self};
 use std::{net::TcpStream, sync::Arc, time::Duration};
 
 /// See [`Listener`](ssh2::Listener).
-pub struct Listener {
+pub struct Listener<'a> {
     inner: ssh2::Listener,
+    inner_session: &'a ssh2::Session,
     stream: Arc<Async<TcpStream>>,
 }
 
-impl Listener {
-    pub(crate) fn new(listener: ssh2::Listener, stream: Arc<Async<TcpStream>>) -> Self {
-        Self {
+impl<'a> Listener<'a> {
+    pub(crate) fn new<'b>(listener: ssh2::Listener, session: &'b ssh2::Session, stream: Arc<Async<TcpStream>>) -> Listener<'b> {
+        Listener {
             inner: listener,
+            inner_session: session,
             stream,
         }
     }
 
     /// See [`accept`](ssh2::Listener::accept).
-    pub async fn accept(&mut self) -> Result<Channel, Error> {
+    pub async fn accept<'b>(&'b mut self) -> Result<Channel<'b>, Error> {
         // The I/O object for Listener::accept is on the remote SSH server. There is no way to poll
         // its state so the best we can do is loop and check whether we have a new connection every
         // 10ms.
@@ -34,6 +36,6 @@ impl Listener {
             Timer::after(Duration::from_millis(10)).await;
         };
 
-        Ok(Channel::new(channel, self.stream.clone()))
+        Ok(Channel::new(channel, self.inner_session, self.stream.clone()))
     }
 }
